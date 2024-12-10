5e265be94d0f9f19891a10993f0b525c9bf43dc9
rkdduss <165152909+rkdduss@users.noreply.github.com>
2024-12-10 23:50:04 +0900

    fix

--- Took/Features/Comment/CommentView.swift
+++ Took/Features/Comment/CommentView.swift
@@ -14,77 +14,82 @@ struct CommentView: View {
     var postId: Int
     
     var body: some View {
-        VStack(alignment:.leading) {
-            HStack {
-                Button {
-                    dismiss()
-                } label: {
-                    Image(systemName: "chevron.left").foregroundColor(.black)
-                        .font(.system(size: 30))
-                        .foregroundColor(.black)
-                        .padding(.bottom,20)
-                }
-                    
-            }
-            Text("댓글 \(viewModel.comment.count)")
-                .font(.system(size: 26).weight(.medium))
-                .padding(.bottom,10)
-
-            Rectangle()
-                .frame(maxWidth: 350,maxHeight: 1)
-                .foregroundColor(.secondary.opacity(0.6))
-                .padding(.bottom,30)
-            ScrollView {
-                LazyVStack(alignment:.leading) {
-                    HStack(alignment:.top) {
-                        Image(systemName: "person.crop.circle")
-                            .font(.system(size: 24))
-                            .foregroundColor(.secondary)
-                            .padding(.trailing,10)
-                        VStack {
-                            RoundedRectangle(cornerRadius: 14)
-                                .stroke(lineWidth: 0.7)
-                                .frame(width: 300,height: 100)
-                                .foregroundColor(.secondary)
-                                .overlay {
-                                    TextEditor(text: $comment)
-                                        .frame(width: 290,height: 90)
-                                }
-                                
-                            Button {
-                                print("click")
-                            } label: {
-                                RoundedRectangle(cornerRadius: 4)
-                                    .frame(width: 300,height: 35)
-                                    .foregroundColor(.requestButton)
-                                    .overlay {
-                                        Text("댓글 달기")
-                                            .font(.system(size: 12).weight(.medium))
-                                            .foregroundColor(.color)
-                                    }
-                            }
-
-                        }
-                    }
-                    VStack(alignment:.leading) {
-                        ForEach(viewModel.comment) { comments in
-                            CommentBox(comment: comments)
-                        }
-                    }
-                    
-                }
+        VStack {
+            ForEach(viewModel.comment) { comments in
+                Text(comments.createdAt)
             }
         }
-        .padding(.horizontal,20)
-        .onAppear {
-            viewModel.fetchComments(postId: postId) { success in
-                if success {
-                    print("댓글 로드 성공")
-                } else {
-                    print("댓글 로드 실패")
-                }
-            }
-        }
+//        VStack(alignment:.leading) {
+//            HStack {
+//                Button {
+//                    dismiss()
+//                } label: {
+//                    Image(systemName: "chevron.left").foregroundColor(.black)
+//                        .font(.system(size: 30))
+//                        .foregroundColor(.black)
+//                        .padding(.bottom,20)
+//                }
+//                    
+//            }
+//            Text("댓글 \(viewModel.comment.count)")
+//                .font(.system(size: 26).weight(.medium))
+//                .padding(.bottom,10)
+//
+//            Rectangle()
+//                .frame(maxWidth: 350,maxHeight: 1)
+//                .foregroundColor(.secondary.opacity(0.6))
+//                .padding(.bottom,30)
+//            ScrollView {
+//                LazyVStack(alignment:.leading) {
+//                    HStack(alignment:.top) {
+//                        Image(systemName: "person.crop.circle")
+//                            .font(.system(size: 24))
+//                            .foregroundColor(.secondary)
+//                            .padding(.trailing,10)
+//                        VStack {
+//                            RoundedRectangle(cornerRadius: 14)
+//                                .stroke(lineWidth: 0.7)
+//                                .frame(width: 300,height: 100)
+//                                .foregroundColor(.secondary)
+//                                .overlay {
+//                                    TextEditor(text: $comment)
+//                                        .frame(width: 290,height: 90)
+//                                }
+//                                
+//                            Button {
+//                                print("click")
+//                            } label: {
+//                                RoundedRectangle(cornerRadius: 4)
+//                                    .frame(width: 300,height: 35)
+//                                    .foregroundColor(.requestButton)
+//                                    .overlay {
+//                                        Text("댓글 달기")
+//                                            .font(.system(size: 12).weight(.medium))
+//                                            .foregroundColor(.color)
+//                                    }
+//                            }
+//
+//                        }
+//                    }
+//                    VStack(alignment:.leading) {
+//                        ForEach(viewModel.comment) { comments in
+//                            CommentBox(comment: comments)
+//                        }
+//                    }
+//                    
+//                }
+//            }
+//        }
+//        .padding(.horizontal,20)
+//        .onAppear {
+//            viewModel.fetchComments(postId: postId) { success in
+//                if success {
+//                    print("댓글 로드 성공")
+//                } else {
+//                    print("댓글 로드 실패")
+//                }
+//            }
+//        }
     }
 }
                    
