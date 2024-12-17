0cc855d4427f420805745a38e38306a7825841ac
rkdduss <165152909+rkdduss@users.noreply.github.com>
2024-12-17 22:41:24 +0900

    fix

--- Took/Features/Comment/CommentView.swift
+++ Took/Features/Comment/CommentView.swift
@@ -22,6 +22,7 @@ struct CommentView: View {
                 ScrollView {
                     LazyVStack {
                         
+                        
                     }
                 }
             }
